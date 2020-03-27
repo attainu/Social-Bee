f8dfbc05f6fd6a202e80697efd5eb7f85aae1d2e
piyush-mahapatra-au6 <piyushmahapatra001@gmail.com>
2020-03-27 16:20:58 +0530

    Added bcryptjs and jwt and completed the userRegister successfully,stored the token in database

--- models/user_models/User.js
+++ models/user_models/User.js
@@ -1,8 +1,10 @@
 //importing the required packages
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     required: [true, 'Please provide your name']
@@ -22,15 +24,60 @@ const UserSchema = new Schema({
     minlength: 6,
     select: false
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
-//exporting the schema
-module.exports = mongoose.model('User', UserSchema);
+// Mongoose middleware is being used before Saving the User data with the JWT TOKEN in DB.
 
+userSchema.pre('save', async function(next) {
+  // Hash the password before saving the user model
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.generateAuthToken = async function() {
+  // Generate an auth token for the user
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  // Search for a user by email and password.
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error({ error: 'Invalid login credentials' });
+  }
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  if (!isPasswordMatch) {
+    throw new Error({ error: 'Invalid login credentials' });
+  }
+  return user;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+//exporting the schema
+module.exports = mongoose.model('User', userSchema);
 
 /*Note to atanu
 Can we make a single User Schema for both Admin and User?
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+const route = require('./routes/user');
+
 //importing the DB connection localhost/Atlas in the server file
 const connectDB = require('./config/db');
 
@@ -21,7 +23,10 @@ const app = express();
 
 //setting up the middlewares
 app.use(morgan('dev'));
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', route);
 
 //setting up the server port for listening
 app.listen(PORT, () => {
